814b3b196d1fe23767757b0a74c2f294e6ff2106
Gavassinha9 <134518907+Gavassinha9@users.noreply.github.com>
2023-10-18 15:41:00 -0300

    Add files via upload

---
+++ Ominitrix-main/Ominitrix-main/scripts.js
@@ -0,0 +1,38 @@
+/*Código de alteração de estilo entre as páginas web*/
+
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) =>
+window
+
+.getComputedStyle(element)
+.getPropertyValue(style)
+
+const initialColors = {
+    fundo: getStyle(html, "--fundo"),
+    visor: getStyle(html, "--visor"),
+    padrao: getStyle(html, "--padrao"),
+    espera: getStyle(html, "--espera")
+}
+const albedoColors = {
+    fundo: getStyle(html, "--fundoAlbedo"),
+    visor: getStyle(html, "--visorAlbedo"),
+    padrao: getStyle(html, "--padraoAlbedo"),
+    espera: getStyle(html, "--esperaAlbedo")
+}
+
+
+const transFormKey = Key =>
+"--" + Key.replace(/([A-Z])/, "$1")
+
+const changeColors = (colors) =>{
+    Object.keys(colors).map(key =>{
+        html.style.setProperty(transFormKey(key), colors[key])
+    })
+}
+
+    checkbox.addEventListener("change", ({target}) =>{
+        target.checked ?
+        changeColors(albedoColors) : changeColors(initialColors)
+    })
